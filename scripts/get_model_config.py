d732c52616f72f35766189c5b0adfac23f705c51
Vishal Padia <vishalpadia9@gmail.com>
2026-07-20 05:16:45 +0000

    print DAC config and decoder layers

--- scripts/get_model_config.py
+++ scripts/get_model_config.py
@@ -3,4 +3,11 @@ from zonos.utils import DEFAULT_DEVICE as device
 
 model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-hybrid", device=device)
 
+print("Model config:")
 print(model.config)
+
+print("DAC config:")
+print(model.autoencoder.dac.config)
+
+print("Decoder layers:")
+print(model.autoencoder.dac.decoder)
